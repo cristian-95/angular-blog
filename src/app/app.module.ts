b69d320e476af5e7581da0fef46e6a62558bee2d
cristian <nem.cristianrc@gmail.com>
2023-10-14 11:34:00 -0300

    components criados e estilizados

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { CardComponent } from './components/card/card.component';
+import { CoverCardComponent } from './components/cover-card/cover-card.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuBarComponent,
+    TitleComponent,
+    CardComponent,
+    CoverCardComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/cover-card/cover-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cover-card',
+  templateUrl: './cover-card.component.html',
+  styleUrls: ['./cover-card.component.css']
+})
+export class CoverCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
